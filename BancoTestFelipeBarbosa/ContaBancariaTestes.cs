aaa5d7d58a9c87ba3134284a2956433c2ea81833
Felipe1501 <felipebsantos1501@gmail.com>
2022-06-05 14:55:34 -0300

    Adicionar arquivos de projeto.

---
+++ BancoTestFelipeBarbosa/ContaBancariaTestes.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BancoTestFelipeBarbosa
+{
+	[TestClass]
+	public class ContaBancariaTestes
+	{
+		[TestMethod]
+		public void Debito_ComValorValido_Atualizado()
+		{
+			//arranjo
+			double saldoInicial = 11.99;
+			double valorDebito = 4.55; //(Fez Um Saque)
+			double saldoEsperado = 7.44; //(Este numero vai ser comparado com o da conta e deve diminuir não subir)
+
+			BancoFelipeBarbosa.ContaBancaria conta = new BancoFelipeBarbosa.ContaBancaria("Moura 2ML1: ", saldoInicial);
+
+			//debita
+			conta.Debito(valorDebito);
+
+			//assert
+			double saldoAtual = conta.Saldo;
+			Assert.AreEqual(saldoEsperado, saldoAtual, 0.001, "CONTA NÃO FOI DEBITADA CORRETAMENTE");
+			{
+			}
+		}
+	}
+}
---
+++ BancoTestFelipeBarbosa/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BancoTestFelipeBarbosa")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BancoTestFelipeBarbosa")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e95e81f-e7c3-4fc8-93a1-60f63120e2dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
